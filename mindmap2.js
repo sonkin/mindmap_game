eceb1e2a4066e85b99f95f476a4fc12f564472f7
Vladimir Sonkin <vsonkin@luxoft.com>
2023-04-12 04:55:04 +0200

    updated mindmap tasks

--- mindmap2.js
+++ mindmap2.js
@@ -11,19 +11,19 @@ const allMindMapNodes = {
 ],
 "Creating a Spring Bean":[
    "Can be done manually in method marked as @Bean",
-   "Possible to do by marking class as @Component, etc.",
+   "Executed when class marked as @Component, etc.",
 ],
 "Configuration":[
-    "Uses conventions over configuration principle",
+    "Uses convention over configuration principle",
 ],
 "POM configuration":[
-    "defines dependencies of application"
+    "Defines the dependencies of the application"
 ],
 "properties configuration":[
-    "can be replaced by YAML configuration"
+    "Can be replaced by YAML configuration"
 ],
 "@ComponentScan":[
-   "Finds all beans marked as @Component, @Service, etc.",
+   "Allows to find all beans marked as @Component, @Service, etc.",
 ],
 "@Service":[
    "Specialized @Component for business logic",
@@ -36,7 +36,7 @@ const allMindMapNodes = {
    "Activating only a subset of the beans",
 ],
 "@Profile":[
-   "Marks the @Bean or class which should be activated conditionally",
+   "Marks Spring Bean or class which should be activated conditionally",
 ],
 "Activating a profile":[
    "Using -Dspring.profiles.active command line flag",
@@ -50,17 +50,17 @@ const allMindMapNodes = {
     "Configures Spring based on dependencies in pom.xml"
 ],
 "Spring Boot Starters":[
-    "Bundle with several dependencies",
+    "Bundles with several dependencies",
 ],
 "spring-boot-starter-parent":[
-    "Includes dependency management for all possible dependencies",
-    "Defines versions for all dependencies",
+    "Defines dependency management for all possible dependencies",
+    "Defines versions for all possible dependencies",
 ],
 "@EnableAutoConfiguration":[
    "Configures Spring based on dependencies in pom.xml",
 ],
 "application.properties":[
-   "Located in any folder in the classpath",
+   "Can be placed to any folder in the classpath",
 ],
 "@PropertySource":[
    "Used to specify the path to the additional application settings file",
