c2efc317157b415fbc005d944c724a18782e3b5d
Vladimir Sonkin <vsonkin@luxoft.com>
2023-04-12 04:30:49 +0200

    results table

---
+++ results.js
@@ -0,0 +1,31 @@
+const API_URL = "https://jquiz-athjd4btb4c0fadd.z01.azurefd.net/100/users";
+const reloadBtn = document.getElementById("reload");
+const quizDataTable = document.getElementById("quizDataTable");
+const tbody = quizDataTable.getElementsByTagName("tbody")[0];
+
+function loadData() {
+    fetch(API_URL)
+        .then((response) => response.json())
+        .then((data) => {
+            renderTable(data);
+        })
+        .catch((error) => {
+            console.error("Error fetching data:", error);
+        });
+}
+
+function renderTable(data) {
+    tbody.innerHTML = "";
+    data.forEach((item) => {
+        const row = tbody.insertRow();
+        row.insertCell().innerHTML = item.name;
+        row.insertCell().innerHTML = item.score;
+        row.insertCell().innerHTML = item.nodesOrdered;
+        row.insertCell().innerHTML = item.attempts;
+        row.insertCell().innerHTML = item.progress;
+    });
+}
+
+reloadBtn.addEventListener("click", loadData);
+loadData();
+setInterval(loadData, 10000);
